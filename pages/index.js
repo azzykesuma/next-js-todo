8d24517d15d1669838ffd096ccb1f3bc64e2954e
azzy kesuma <official.azzy@gmail.com>
2022-04-18 08:28:56 +0700

    adding delete btn

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,33 @@ import { Container } from "@mui/material"
 import { Typography } from "@mui/material"
 import Link from "next/link"
 import Box from '@mui/material/Box';
+import { Avatar, Paper } from '@mui/material/';
+// icons
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export const getStaticProps = async () => {
   const res = await fetch('http://localhost:8000/acts')
   const data = await res.json()
 
   return {
-    props : {todos : data}
+    props : {todos : data},
+    revalidate : 1
   }
 }
 
 
 export default function Home({todos}) {
+
+  const HandleDelete = async (id) => {
+    await fetch('http://localhost:8000/acts/' + id , {
+      method : 'DELETE'
+    })
+
+    const newTodo = todos.filter(todo => todo.id !== id);
+    return {
+      props : {todos : newTodo}
+    }
+  }
 
   return (
     <>
@@ -39,19 +54,66 @@ export default function Home({todos}) {
             <div key={item.id}>
               <Box
               sx = {{
-                backgroundColor: '#efebe9',
                 padding : '10px',
                 marginBottom : '10px',
                 marginTop : '10px',
-                borderRadius : '5px'
+                borderRadius : '5px',
+                border : '1px solid #e0e0e0',
+                display : 'flex',
+                alignItems : 'center',
+                justifyContent : 'space-between',
+                padding : '10px;',
               }}
+              className={item.tags}
               >
-                <Typography
-                variant="body"
-                color="secondary"
+                <Box
+                sx = {{
+                  display : 'flex',
+                  flexDirection : 'column',
+                  gap : '10px'
+                }}
                 >
-                  {item.act}
-                </Typography>
+                  <Avatar>
+                    {item.tags[0].toUpperCase()}
+                  </Avatar>
+                  <Typography
+                  variant="body"
+                  className={item.tags}
+                  fontFamily={'Roboto'}
+                  >
+                    {item.act}
+                  </Typography>
+                </Box>
+                <Box
+                sx = {{
+                  display : 'flex',
+                  gap : '5px'
+                }}
+                >
+                  <Paper
+                  className = 'paperTags'
+                  >{item.tags}</Paper>
+                  <Paper
+                  className = 'paperTags'
+                  >{item.priority}</Paper>
+                </Box>
+                <Box
+                sx = {{
+                  backgroundColor : '#B20600',
+                  borderRadius : '50%',
+                  padding : '5px',
+                  display : 'flex',
+                  alignItems : 'center',
+                  justifyContent : 'center'
+                }}
+                >
+                  <DeleteIcon
+                  className="deleteBtn"
+                  cursor="pointer"
+                  onClick={() => HandleDelete(item.id)}
+                  />
+
+                </Box>
               </Box>
             </div>
           ))
