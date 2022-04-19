48ae7cbd9af462bac485e480b0965a3eab1926ba
azzy kesuma <official.azzy@gmail.com>
2022-04-19 07:51:34 +0700

    removing import fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,7 @@ const Document = () => {
     return (
         <Html>
             <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet" />
+            <title>my todo app</title>
             </Head>
             <body>
                 <Main />
